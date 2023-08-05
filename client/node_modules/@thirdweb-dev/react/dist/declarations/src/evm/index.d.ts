6cf7ba170ebd12c08645b02225243b9a624fd5d2
Malhar Kulkarni <malharkulkarni619@gmail.com>
2023-08-05 16:31:19 +0530

    Files Upload

---
+++ client/node_modules/@thirdweb-dev/react/dist/declarations/src/evm/index.d.ts
@@ -0,0 +1,14 @@
+export * from "./providers/full";
+export * from "./hooks/wagmi-required/useAccount";
+export * from "./hooks/wagmi-required/useNetwork";
+export * from "./hooks/wagmi-required/useDisconnect";
+export * from "./hooks/wagmi-required/useConnect";
+export * from "./hooks/connectors/useMetamask";
+export * from "./hooks/connectors/useWalletConnect";
+export * from "./hooks/connectors/useWalletLink";
+export * from "./components/MediaRenderer";
+export * from "./components/NftMedia";
+export * from "./components/ConnectWallet";
+export * from "./components/Web3Button";
+export * from "@thirdweb-dev/react-core/evm";
+//# sourceMappingURL=index.d.ts.map
